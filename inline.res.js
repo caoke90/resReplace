f3f7a72ba8f10076ee6bcebb72a520617fc53303
caoke90 <914890674@qq.com>
2016-11-22 22:42:09 +0800

    d

--- inline.res.js
+++ inline.res.js
@@ -1,16 +1,22 @@
 var fs=require("fs")
 var path=require("path")
-var inlineReplace=function(){
-    var reg=/(.*)__inline\((.+?)\)/g
+var inlineReplace=function(type){
+    var type=type||"inline";
+    var data={
+        "inline":/\s__inline\((.+?)\)/g,
+        "txt":/(?=.+)__inline\((.+?)\)/g
+    }
+    var reg=data[type];
     var replace=function (content,filepath,callback) {
         var sync=function(content,filepath){
             if(reg.test(content)){
                 var dirname=path.dirname(filepath)+"/"
-                content=content.replace(reg,function(m,p1,p2){
-                    var path1=p2.replace(/["']/g,"");
+                content=content.replace(reg,function(m,p1){
+                    console.log(p1)
+                    var path1=p1.replace(/["']/g,"");
                     var filepath1=path.join(dirname+path1)
                     var str=fs.readFileSync(filepath1).toString()
-                    if(p1){
+                    if(type=="txt"){
                         return "'"+sync(str,filepath1).replace(/\s/g,"").replace(/'/g,'\\')+"'";
                     }else{
                         return sync(str,filepath1);

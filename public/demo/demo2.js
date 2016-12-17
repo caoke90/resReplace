f24d97732d415f859d0aab6388d01b15e57cd91f
caoke <caoke@qq.com>
2016-12-17 19:01:38 +0800

    d

--- public/demo/demo2.js
+++ public/demo/demo2.js
@@ -6,7 +6,7 @@ var Demo=Api.BigClass.extend({
     getData:eval(Wind.compile("async", function (req,next) {
         var content=$await(Api.getContent("http://oil.cngold.com.cn/20161130d1816n103335895.html"))
         var info=Api.getInfo(content)
-        next("21212")
+        next(info)
     })),
     getCache:eval(Wind.compile("async", function (req,next) {
         next("212222212")

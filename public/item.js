e8376da3223cdde2cb39df7c176b4c0b47d6cca4
caoke90 <914890674@qq.com>
2016-12-19 21:34:54 +0800

    d

--- public/item.js
+++ public/item.js
@@ -5,6 +5,9 @@ module.exports={
     render:eval(Wind.compile("async", function (req,next) {
         var tid=req.query.tid
         var content=$await(Api.localstore.getAsync(tid))
+        if(!content){
+            content=""
+        }
         content=content.replace(/href="forum.php/g,'href="http://www.168ytt.com/forum.php')
         content=content.replace(/src="forum.php/g,'src="http://www.168ytt.com/forum.php')
         next(content)
--- public/list.js
+++ public/list.js
@@ -1,9 +1,13 @@
 var glob=require("glob")
 var Wind=require("Wind")
+var fs=require("fs")
 var Api=require("../Api")
 module.exports={
     render:eval(Wind.compile("async", function (req,next) {
-        var list=$await(Api.localstore.keysAsync("*"))
-        next(list)
+
+        if(fs.existsSync("tidData.txt")){
+            var taskData=JSON.parse(fs.readFileSync("tidData.txt").toString())
+        }
+        next(taskData)
     }))
 }
